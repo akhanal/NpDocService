00acdf896fb2974a255567af33afce18b8ed8f8c
Ankit <ankit@yasoksystems.com>
2024-07-21 21:57:37 +0545

    allow cors for testing

--- src/main/java/co/yasok/npdoc/config/CorsConfig.java
+++ src/main/java/co/yasok/npdoc/config/CorsConfig.java
@@ -1,6 +1,6 @@
 package co.yasok.npdoc.config;
-
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,8 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8081")
-                .allowedMethods("GET", "POST", "HEAD");  // or use '*' to allow all methods
+        registry.addMapping("/api/**") // Apply to all endpoints
+                .allowedOrigins("*") // Allow all origins
+                .allowedMethods("*") // Allow all methods (GET, POST, etc.)
+                .allowedHeaders("*"); // Allow all headers
     }
 }
