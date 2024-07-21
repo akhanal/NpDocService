a65dd8dbe7422a3975fe2da459d444c4aac099fb
Ankit <ankit@yasoksystems.com>
2024-07-21 21:11:16 +0545

    change to make things compatible with client

--- src/main/java/co/yasok/npdoc/websocket/SignalingHandler.java
+++ src/main/java/co/yasok/npdoc/websocket/SignalingHandler.java
@@ -5,30 +5,34 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.socket.*;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class SignalingHandler extends TextWebSocketHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(SignalingHandler.class);
     private final Map<String, WebSocketSession> sessionMap = new ConcurrentHashMap<>();
-    private final Map<String, String> userSessionMap = new ConcurrentHashMap<>();
-    private final Map<String, String> activeCalls = new ConcurrentHashMap<>();
+    private final Map<Integer, String> userSessionMap = new ConcurrentHashMap<>();
+    private final Map<Integer, Integer> activeCalls = new ConcurrentHashMap<>();
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         String userId = session.getUri().getQuery().split("=")[1];
+        logger.info("uri query = " + session.getUri().getQuery());
         sessionMap.put(session.getId(), session);
-        userSessionMap.put(userId, session.getId());
+        userSessionMap.put(Integer.parseInt(userId), session.getId());
         System.out.println("Connection established for user: " + userId);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         String sessionId = session.getId();
-        String userId = userSessionMap.entrySet().stream()
+        Integer userId = userSessionMap.entrySet().stream()
                 .filter(entry -> entry.getValue().equals(sessionId))
                 .map(Map.Entry::getKey)
                 .findFirst()
@@ -37,7 +41,7 @@ public class SignalingHandler extends TextWebSocketHandler {
         if (userId != null) {
             userSessionMap.remove(userId);
             sessionMap.remove(sessionId);
-            String targetUserId = activeCalls.remove(userId);
+            Integer targetUserId = activeCalls.remove(userId);
             if (targetUserId != null) {
                 sendCallEndedMessage(targetUserId, userId);
                 activeCalls.remove(targetUserId);
@@ -50,30 +54,32 @@ public class SignalingHandler extends TextWebSocketHandler {
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
         Map<String, Object> messageData = objectMapper.readValue(payload, Map.class);
-
+        logger.info("Received text message: " + payload);
         String messageType = (String) messageData.get("type");
-        String userId = (String) messageData.get("userId");
-        String targetUserId = (String) messageData.get("targetUserId");
-
-        switch (messageType) {
-            case "startCall":
-                startCall(userId, targetUserId);
-                break;
-            case "endCall":
-                endCall(userId, targetUserId);
-                break;
-            case "message":
-            case "callOffer":
-            case "answer":
-            case "iceCandidate":
-                forwardMessage(targetUserId, messageData);
-                break;
-            default:
-                break;
+        Integer userId = (Integer) messageData.get("userId");
+        Integer targetUserId = (Integer) messageData.get("targetUserId");
+        logger.info("UserId = " + userId + ", targetUserId = " + targetUserId);
+        if(userId != null && targetUserId != null) {
+            switch (messageType) {
+                case "startCall":
+                    startCall(userId, targetUserId);
+                    break;
+                case "endCall":
+                    endCall(userId, targetUserId);
+                    break;
+                case "message":
+                case "callOffer":
+                case "answer":
+                case "iceCandidate":
+                    forwardMessage(targetUserId, messageData);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
-    private void startCall(String initiatingUser, String targetUser) {
+    private void startCall(Integer initiatingUser, Integer targetUser) {
         activeCalls.put(initiatingUser, targetUser);
         activeCalls.put(targetUser, initiatingUser);
         sendMessageToUser(targetUser, Map.of(
@@ -83,30 +89,32 @@ public class SignalingHandler extends TextWebSocketHandler {
         System.out.println("Call started between " + initiatingUser + " and " + targetUser);
     }
 
-    private void endCall(String userId, String targetUserId) {
+    private void endCall(Integer userId, Integer targetUserId) {
         activeCalls.remove(userId);
         activeCalls.remove(targetUserId);
         sendCallEndedMessage(targetUserId, userId);
         System.out.println("Call ended between " + userId + " and " + targetUserId);
     }
 
-    private void forwardMessage(String targetUserId, Map<String, Object> messageData) {
+    private void forwardMessage(Integer targetUserId, Map<String, Object> messageData) {
         sendMessageToUser(targetUserId, messageData);
     }
 
-    private void sendMessageToUser(String userId, Map<String, Object> messageData) {
+    private void sendMessageToUser(Integer userId, Map<String, Object> messageData) {
         String sessionId = userSessionMap.get(userId);
-        WebSocketSession session = sessionMap.get(sessionId);
-        if (session != null && session.isOpen()) {
-            try {
-                session.sendMessage(new TextMessage(objectMapper.writeValueAsString(messageData)));
-            } catch (Exception e) {
-                e.printStackTrace();
+        if(sessionId != null) {
+            WebSocketSession session = sessionMap.get(sessionId);
+            if (session != null && session.isOpen()) {
+                try {
+                    session.sendMessage(new TextMessage(objectMapper.writeValueAsString(messageData)));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
-    private void sendCallEndedMessage(String targetUserId, String userId) {
+    private void sendCallEndedMessage(Integer targetUserId, Integer userId) {
         sendMessageToUser(targetUserId, Map.of(
                 "type", "callEnded",
                 "userId", userId
